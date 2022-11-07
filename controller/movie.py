6d46492e5f2a981b46a5eb6bf3066aca52cd8749
alejandroruiz23 <102110186+alejandroruiz23@users.noreply.github.com>
2022-11-06 22:05:58 -0500

    se empiezan a crear rutas de las apis

---
+++ controller/movie.py
@@ -0,0 +1,22 @@
+from app import app
+from model.repository import MovieRepository
+from model.entity import Movie
+
+repository = MovieRepository()
+
+
+@app.route("/api/movies", methods =["GET"])
+
+def list():
+    return repository.findAll()
+
+@app.route("/api/movies/<code>", methods = ["GET"])
+def findById(code):
+    return repository.findByCode(code)
+
+@app.route("/api/movies", methods = ["POST"])
+def create():
+    movie = Movie()
+    repository.insert(movie)
+    
+    
